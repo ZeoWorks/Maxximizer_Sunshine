6c12d48732b16149aba3cb87d4a5897612298ea6
ZeoWorks <zeoworks@hotmail.co.uk>
2023-08-24 18:55:55 +0100

    Remove build folder

--- build/CMakeFiles/sunshine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sunshine.
-# This may be replaced when dependencies are built.
--- build/third-party/moonlight-common-c/enet/CMakeFiles/enet.dir/win32.c.o.d
+++
@@ -1,3 +0,0 @@
-third-party/moonlight-common-c/enet/CMakeFiles/enet.dir/win32.c.o: \
- /media/sean/Data/MaximumSettings/Linux_Sunshine/sunshine/third-party/moonlight-common-c/enet/win32.c \
- /usr/include/stdc-predef.h
